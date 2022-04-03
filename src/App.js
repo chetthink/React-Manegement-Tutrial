04e42f72c61a139597fc274a99982be4de4de936
chetthink <cksdn7199@gmail.com>
2022-04-03 17:13:08 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="gray-background">
-     <img src ={logo} lat ="logo" />
-     <h2>Let`s develop manegement system</h2>
-    </div>
-  );
+const customers = [
+  {
+  'id' : '1',
+  'image' : 'https://placeimg.com/64/64/any/1',
+  'name' : '고찬우',
+  'birthday':'961018',
+  'gender' : '남자',
+  'job': '대학생'
+},
+{
+  'id' : '2',
+  'image' : 'https://placeimg.com/64/64/any/2',
+  'name' : '조현아',
+  'birthday':'960522',
+  'gender' : '여자',
+  'job': '치위생사'
+},
+{
+  'id' : '3',
+  'image' : 'https://placeimg.com/64/64/any/3',
+  'name' : '케이크',
+  'birthday':'unknown',
+  'gender' : '무생물',
+  'job': '디저트'
+}
+]
+class App extends Component {
+  render(){
+    return(
+      <div>
+       {
+         customers.map(c => {
+           return (
+             <Customer
+             key={c.id}
+             id={c.id}
+             image={c.image}
+             name={c.name}
+             birthday={c.birthday}
+             gender={c.gender}
+             job={c.job}
+           />
+             );
+         })
+       }
+      </div>
+    );
+  }
 }
 export default App;
